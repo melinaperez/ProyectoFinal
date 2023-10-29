e174728e7c1a6e3ffdd1540932f1d42276094579
melinaperez <myperez@garantizar.com.ar>
2023-10-29 20:47:10 -0300

    Students details

--- src/app/dashboard/pages/students/components/students-details/students-details.component.ts
+++ src/app/dashboard/pages/students/components/students-details/students-details.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Student } from '../../models';
+import { ActivatedRoute } from '@angular/router';
+import { StudentsService } from '../../students.service';
 
 @Component({
   selector: 'app-students-details',
   templateUrl: './students-details.component.html',
-  styleUrls: ['./students-details.component.scss']
+  styleUrls: ['./students-details.component.scss'],
 })
 export class StudentsDetailsComponent {
+  student: Student | undefined;
+  id: number;
 
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private studentsService: StudentsService
+  ) {
+    this.id = parseInt(this.activatedRoute.snapshot.params['id']);
+    this.studentsService.getStudentById$(this.id).subscribe((student) => {
+      this.student = student;
+    });
+  }
+
+  onBack(): void {
+    window.history.back();
+  }
 }
