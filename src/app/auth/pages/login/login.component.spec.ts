44b5118e6e49c8871ddf12651a73454556a6655b
melinaperez <myperez@garantizar.com.ar>
2023-11-08 21:49:49 -0300

    Unit test

---
+++ src/app/auth/pages/login/login.component.spec.ts
@@ -0,0 +1,85 @@
+// import { TestBed } from '@angular/core/testing';
+// import { LoginComponent } from './login.component';
+// import { SharedModule } from 'src/app/shared/shared.module';
+
+// describe('Testing sobre logueo', () => {
+//   let loginComponent: LoginComponent;
+
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [LoginComponent],
+//       imports: [SharedModule],
+//     });
+
+//     loginComponent = TestBed.createComponent(LoginComponent).componentInstance;
+//   });
+
+//   it('should create login component', () => {
+
+//     expect(loginComponent).toBeTruthy();
+//   });
+
+//   it('Debe marcar todos los campos del form como touched si este es inválido', () => {
+//     loginComponent.login();
+//     expect(loginComponent.loginForm.controls).toBeTrue();
+//   });
+// });
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, SharedModule],
+      providers: [AuthService],
+    });
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+  });
+
+  //Compruebo que el componente se crea correctamente.
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  //Compruebo que el formulario de inicio de sesión se inicializa correctamente con email y contraseña.
+  it('should initialize the login form with email and password controls', () => {
+    expect(component.loginForm.get('email')).toBeTruthy();
+    expect(component.loginForm.get('password')).toBeTruthy();
+  });
+
+  //Compruebo que, si el formulario es inválido, se marcan todos los campos como toched después de llamar a login().
+  it('should mark all form controls as touched if the form is invalid', () => {
+    const emailControl = component.loginForm.get('email');
+    emailControl?.setValue('invalid-email');
+    component.login();
+    expect(emailControl?.touched).toBe(true);
+  });
+
+  //Compruebo que authService.login se llama correctamente cuando el formulario es válido.
+  it('should call authService.login when the form is valid', () => {
+    const authServiceSpy = spyOn(authService, 'login');
+    const emailControl = component.loginForm.get('email');
+    const passwordControl = component.loginForm.get('password');
+    emailControl?.setValue('test@example.com');
+    passwordControl?.setValue('testpassword');
+    component.login();
+    expect(authServiceSpy).toHaveBeenCalledWith({
+      email: 'test@example.com',
+      password: 'testpassword',
+    });
+  });
+});
---
+++ src/app/auth/services/auth.service.spec.ts
@@ -0,0 +1,84 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from './auth.service';
+import { User } from 'src/app/dashboard/pages/users/models/user.models';
+import { environment } from 'src/environments/environment.local';
+
+import { MockProvider } from 'ng-mocks';
+import { Router } from '@angular/router';
+
+describe('AuthService', () => {
+  let authService: AuthService;
+  let httpController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [MockProvider(Router)],
+    });
+
+    authService = TestBed.inject(AuthService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it('AuthService should be defined', () => {
+    expect(authService).toBeTruthy();
+  });
+
+  //Compruebo auntenticación con usuario
+  it('should be set an authenticated user when doing login()', () => {
+    const USER_MOCK: User = {
+      id: 1,
+      email: 'fake@mail.com',
+      lastname: 'fakeLastName',
+      name: 'fakeName',
+      token: '84jfdskfsdjh3m2nudisfdusafjd',
+      password: '123456',
+      dni: '36767749',
+      registrationDate: new Date(),
+    };
+
+    authService.login({
+      email: USER_MOCK.email,
+      password: USER_MOCK.password,
+    });
+
+    httpController
+      .expectOne({
+        method: 'GET',
+        url: `${environment.baseUrl}/users?email=${USER_MOCK.email}&password=${USER_MOCK.password}`,
+      })
+      .flush([USER_MOCK]);
+
+    authService.authUser$.subscribe({
+      next: (authUser) => {
+        expect(authUser).toBeTruthy();
+        expect(authUser).toEqual(USER_MOCK);
+      },
+    });
+  });
+
+  //Compruebo el token del usuario logueado
+  it('should verify the user token successfully', () => {
+    localStorage.setItem('token', 'test-token');
+    let authUser: User | null = null;
+
+    authService.authUser$.subscribe((user) => {
+      authUser = user;
+    });
+
+    authService.verifyToken().subscribe((result) => {
+      expect(result).toBe(true);
+    });
+
+    const req = httpController.expectOne(`${environment.baseUrl}/users?token=test-token`);
+
+    req.flush([{ token: 'test-token' }]);
+
+    expect(authUser).toBeTruthy();
+  });
+});
